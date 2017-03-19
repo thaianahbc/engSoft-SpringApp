5b8d502344bdc23adbd3273230a3cd2b7d543770
Thaiana Hofman do Bom Conselho <thaianahbc@gmail.com>
2017-03-19 11:05:14 -0300

    Criando classes do Backend

---
+++ EmployeeApp/src/main/java/com/work/Employee.java
@@ -0,0 +1,29 @@
+package com.work;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+/**
+ * @author Thaiana H.B.C
+ */
+
+@Data
+@Entity
+public class Employee {
+
+	private @Id @GeneratedValue Long id;
+	private String firstName;
+	private String lastName;
+	private String description;
+
+	private Employee() {}
+
+	public Employee(String firstName, String lastName, String description) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.description = description;
+	}
+}
---
+++ EmployeeApp/src/main/java/com/work/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.work;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * 
+ * @author Thaiana H.B.C
+ *
+ */
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
